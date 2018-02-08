53dd7162684502e96494d73d2dd4177da5f6781a
Noah <noahirwin2016@gmail.com>
2018-02-08 14:06:03 +0000

    Added couts for binary

--- Search.h
+++ Search.h
@@ -30,14 +30,20 @@ template <class T>
 int binarySearch(const Array<T> & items, T key) {
 	int left = 0, right = items.length() - 1;
 
+	std::cout << "Searching for: " << key << std::endl;
+	std::cout << "In Array: " << std::endl;
+	for (int i = 0; i < items.length(); i++) {
+		std::cout << items[i] << ", ";
+	}
+	std::cout << "\n";
 
 	while (left <= right) {
 		int pivot = (left + right) / 2;
 
-		if (searchKey == items[pivot]) {
+		if (key == items[pivot]) {
 			return pivot;	    // found
 		}
-		else if (searchKey > items[pivot]) {
+		else if (key > items[pivot]) {
 			left = pivot + 1; 	// search to right
 		}
 		else {
@@ -50,20 +56,26 @@ int binarySearch(const Array<T> & items, T key) {
 // PreCondition: Data array is full
 // PostCondition: return number of occurences of e in Array
 template <class T>
-int countOccurrence( ... ) {
-	
+int countOccurrence(const Array<T> & items, T key) {
+	int count = 0;
+
+	for (int i = 0; i < items.length(); i++) {
+		count++;
+	}
+	return count;
 }
 
 // PreCondition: Data array is full and sorted
 // PostCondition: return number of occurences of e in Array
 template <class T>
-int countOccurrenceOrdered( ... ) {
+int countOccurrenceOrdered(const Array<T> & items, T key) {
 
 }
 
 // PreCondition: Both Arrays are filled with values of type T. 
 //               items - values to be searched. keys - values to search for
 // PostCondition: return true if all keys are found, otherwise false
-bool containsAll(...items, ...keys) { }
+template <class T>
+bool containsAll(const Array<T> & items, T key) { }
 
 #endif	/* SEARCH_H_ */
